0c0e4557a7fde57455ecb22f2e7e8756997ed69b
Aleksandr Trebushchuk <zamsion@yandex.ru>
2020-04-30 14:56:46 +0700

    Added a new clang format, fixed calculation of the number of lines

--- main.cpp
+++ main.cpp
@@ -3,47 +3,49 @@
 #include <limits>
 using namespace std;
 
-int line_length(int num, char **mass) {
-  ifstream line("words.txt", ios::in | ios::binary);
+int line_length(int num, char** mass)
+{
+    ifstream line("words.txt", ios::in | ios::binary);
+    string a;
 
-  line.close();
-  return 0;
-}
-
-int number_of_strings() {
-  ifstream start("words.txt", ios::in | ios::binary);
-  if (!start) {
-    cout << "File with shapes not found, maybe you renamed it" << endl;
+    line.close();
     return 0;
-  }
-
-  int count = 0;
-  while (start.ignore(numeric_limits<streamsize>::max(), '\n')) {
-    if (!start.eof())
-      count++;
-  }
-
-  start.close();
-  return count;
 }
 
-int main() {
-  const int len = 50, strings = number_of_strings();
+int number_of_strings()
+{
+    ifstream start("words.txt", ios::in | ios::binary);
+    if (!start) {
+        cout << "File with shapes not found, maybe you renamed it" << endl;
+        return 0;
+    }
 
-  const char ch = '\n';
-  char **mass = new char *[strings];
-  for (int i = 0; i < strings; i++) {
-    mass[i] = new char[len];
-  }
+    int count = 1;
+    while (start.ignore(numeric_limits<streamsize>::max(), '\n')) {
+        if (!start.eof())
+            count++;
+    }
+    start.close();
+    cout << count;
+    return count;
+}
 
-  ifstream work("words.txt", ios::in | ios::binary);
+int main()
+{
+    const int len = 50, strings = number_of_strings();
 
-  for (int r = 0; r < strings; r++) {
-    work.getline(mass[r], len - 1, ch);
-    cout << "String " << r + 1 << " = " << mass[r] << endl;
-  }
+    const char ch = '\n';
+    char** mass = new char*[strings];
+    for (int i = 0; i < strings; i++) {
+        mass[i] = new char[len];
+    }
 
-  work.close();
+    ifstream work("words.txt", ios::in | ios::binary);
 
-  return 0;
+    for (int r = 0; r < strings; r++) {
+        work.getline(mass[r], len, ch);
+        cout << "String " << r << " = " << mass[r] << endl;
+    }
+    work.close();
+    return 0;
 }
